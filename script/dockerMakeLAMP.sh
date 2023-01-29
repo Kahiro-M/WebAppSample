a9c9850ea65899d02e02cdc92b94b2ebb81884b1
Kahiro-M <0204.kahiro@gmail.com>
2023-01-29 11:21:36 +0900

    実行環境更新

--- script/dockerMakeLAMP.sh
+++ script/dockerMakeLAMP.sh
@@ -6,4 +6,15 @@ sudo apt install docker-compose -y
 # Dockerイメージのビルド,起動
 cd docker-lamp
 sudo docker-compose build
+
+# $ docker-compose upを実行すると、
+# ①Dockerイメージをbuildして、
+# ②runが実行される
+# もし既にDockerイメージがbuildされている場合は、
+# ②のrunだけが実行される。
+# --buildオプションで、buildとrunを一緒に実行
 sudo docker-compose up -d
+
+#Dockerfileの更新を反映させる
+# sudo docker-compose up -d --build
+
