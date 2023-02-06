d0f6e76ea91f7a695f1f9a42d4c20c7a3fb6b176
Kahiro-M <0204.kahiro@gmail.com>
2023-02-06 08:28:06 +0000

    タイムゾーン変更、テーブル修正

--- docker-lamp/cakephp/config/app.php
+++ docker-lamp/cakephp/config/app.php
@@ -284,7 +284,7 @@ return [
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
              */
             //'encoding' => 'utf8mb4',
-            'timezone' => 'UTC',
+            'timezone' => 'Asia/Tokyo',
             'flags' => [],
             'cacheMetadata' => true,
             'log' => false,
